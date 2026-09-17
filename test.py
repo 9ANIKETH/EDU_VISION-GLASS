d9409d3bbfad8a39e86db2bd64b02ec33f239bee
ANIKETH DEY <163969481+9ANIKETH@users.noreply.github.com>
2026-09-17 21:28:34 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-from banglatts import BanglaTTS
-
-tts = BanglaTTS(save_location="save_model_location")
-path = tts("আমি বাংলায় কথা বলতে পারি।", voice='female', filename='1.wav')
-
-os.system(f"aplay {path}")   # ALSA ব্যবহার করে play হবে
